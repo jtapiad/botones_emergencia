e4a86d094b3c749416f7256d2a497ddbfce06b8e
json <json@MacBook-Pro-de-json.local>
2018-11-28 00:37:46 -0300

    se agrega boton

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React, {Component} from 'react';
-import {ImageBackground,Image, TouchableOpacity, Button, Linking, Alert, Platform, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, ImageBackground, Button, Text, Image, TouchableOpacity, StyleSheet, View} from 'react-native';
 import Communications from 'react-native-communications';
-
-const args = {
-  number: '123456', // String value with the number to call
-  prompt: false // Optional boolean property. Determines if the user should be prompt prior to the call 
-}
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 
-
-export default class ButtonBasics extends Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
+  _llamarSegur(){
+    Communications.phonecall('1402', false);
   }
   _llamarAmbulancia(){
     Communications.phonecall('131', false);
@@ -23,40 +21,85 @@ export default class ButtonBasics extends Component {
     Communications.phonecall('134', false);
   }
   _llamarCarabinero(){
-    // RNImmediatePhoneCall.immediatePhoneCall('0123456789');
     Communications.phonecall('133', false);
   }
   render() {
     return (
-
+      
       <ImageBackground source={require('./assets/backg.png')} style={styles.bkg}>
-              <View style={styles.container}>
-                  <TouchableOpacity style={styles.botones} onPress={this._llamarAmbulancia}>
-                    <Image source={require('./assets/ambu.jpg')} style={styles.imagenes}/>
-                  </TouchableOpacity>
+        <ScrollView>
+          <View style={styles.container}>
 
-                 <TouchableOpacity style={styles.botones} onPress={this._llamarBombero}>
-                    <Image source={require('./assets/bomb.png')} style={styles.imagenes}/>
-                  </TouchableOpacity>
+              <TouchableOpacity style={styles.botones} onPress={this._llamarSegur}>
+                <Image source={require('./assets/ciuda.png')} style={styles.imagenes}/>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.botones} onPress={this._llamarAmbulancia}>
+                <Image source={require('./assets/ambu.jpg')} style={styles.imagenes}/>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.botones} onPress={this._llamarBombero}>
+                <Image source={require('./assets/bomb.png')} style={styles.imagenes}/>
+              </TouchableOpacity>
 
 
-                  <TouchableOpacity style={styles.botones} onPress={this._llamarCarabinero}>
-                    <Image source={require('./assets/carab.png')} style={styles.imagenes}/>
-                  </TouchableOpacity>
+              <TouchableOpacity style={styles.botones} onPress={this._llamarCarabinero}>
+                <Image source={require('./assets/carab.png')} style={styles.imagenes}/>
+              </TouchableOpacity>
 
-                  <TouchableOpacity style={styles.botones} onPress={this._llamarPolicia}>
-                    <Image source={require('./assets/pdi.png')} style={styles.imagenes}/>
-                  </TouchableOpacity>
-                  </View>
+              <TouchableOpacity style={styles.botones} onPress={this._llamarPolicia}>
+                <Image source={require('./assets/pdi.png')} style={styles.imagenes}/>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.botones} onPress={() => this.props.navigation.navigate('Details')}>
+                <Image source={require('./assets/chat.png')} style={styles.imagenes}/>
+              </TouchableOpacity>
+          </View>
+        </ScrollView>
       </ImageBackground>
     );
   }
 }
 
+class DetailsScreen extends Component {
+  static navigationOptions = {
+    title: 'Details',
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: 'Home',
+    headerMode:'float',
+    /* The header config from HomeScreen is now here */
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#1E496B',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+);
+
+export default createAppContainer(AppNavigator);
+
 const styles = StyleSheet.create({
   container: {
         // backgroundColor:'red',
-        marginTop: '15%',
         flex: 1,
         flexDirection: 'row',
         flexWrap: 'wrap',
@@ -73,8 +116,7 @@ const styles = StyleSheet.create({
     height:100,
     backgroundColor:'#01a699',
     flexWrap: 'wrap',
-    marginTop: 40,
-    zIndex:10
+    marginTop: '4%',
   },
   imagenes:{
     marginTop:2.5,
